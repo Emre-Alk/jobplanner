68be7066a468105c74d5f55b1bf111923c7fff2d
Emre ALKAN <alkan.emre@hotmail.fr>
2024-03-05 18:16:29 +0100

    response fetch à finir

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,3 +1,29 @@
-class PostsController < Api::V1::BaseController
+class Api::V1::PostsController < Api::V1::BaseController
+  # with this heritage, the user is authentified by token and available as @user
   before_action :verfiy_auth_token
+
+  def create
+    puts "😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀"
+    p params
+
+    @post = Post.new(post_params)
+    @post.user = @user
+    @company = Company.new(name: "framatome")
+    @post.company = @company
+
+    if post.save
+      # QueryAiJob.perform_later # cours de demain
+      puts 'ok'
+      render json: { status: 200, message: 'ok' }
+    else
+      puts 'nok'
+      render json: { status: 422, message: post.errors.full_messages.to_sentence }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :token)
+  end
 end
