7c9a7373c753cd183601f20b84d3a331b3ce419d
Emre ALKAN <alkan.emre@hotmail.fr>
2024-03-05 11:01:44 +0100

    setup - routes root to

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root to: "posts#index"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-  root to: "pages#home"
+  resources :posts, only: %i[index show create edit update]
 end
