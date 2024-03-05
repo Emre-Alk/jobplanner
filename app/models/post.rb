be1ebba29ce3a89f19c8522bd1699c0b6e4bcf49
Emre ALKAN <alkan.emre@hotmail.fr>
2024-03-05 12:44:07 +0100

    api done - status corrected

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   has_many :post_stacks
   belongs_to :company
   belongs_to :user
+
+  enum status: {
+    pending: 0,
+    applied: 10,
+    rejected: 30,
+  }
 end
