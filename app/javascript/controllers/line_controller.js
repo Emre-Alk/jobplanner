f61c68a2bb791a1d20ed1b2890b7e26a04dc1424
Robin Couret <couret.robin@protonmail.com>
2024-03-07 18:11:42 +0100

    chart

---
+++ app/javascript/controllers/line_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables);
+
+// Connects to data-controller="chart"
+export default class extends Controller {
+  static targets = ["line"]
+
+  connect() {
+
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'Applied',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(54, 162, 235)',
+        tension: 0.1
+      }]
+    };
+
+    const chartOptions = {
+      plugins: {
+        legend: {
+          position: 'right' // This positions the legend at the bottom
+        }
+      }
+    };
+
+    new Chart(this.lineTarget, {
+      type: 'line',
+      data: data,
+      options: chartOptions
+    });
+   }
+
+}
