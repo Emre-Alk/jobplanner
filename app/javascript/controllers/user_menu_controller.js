7805ad9b2f1f912fb614c3f1779a36678d607a77
jamal daajna <djamal84@gmail.com>
2024-03-11 14:17:51 +0100

    logout button etc

--- app/javascript/controllers/user_menu_controller.js
+++ app/javascript/controllers/user_menu_controller.js
@@ -1,11 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets =["link"]
+  static targets =["link", "page"]
   show() {
     this.linkTarget.classList.toggle("hidden")
+    this.pageTarget.classList.toggle("hidden")
   }
   connect(){
     console.log("test")
+  }
+  close(event) {
+    console.log(event.target)
+    this.linkTarget.classList.add("hidden")
+    this.pageTarget.classList.toggle("hidden")
   }
 }
