8aec28ef0a7d33e66c9f456eee3368198b47aa29
jamal daajna <djamal84@gmail.com>
2024-03-14 16:37:12 +0100

    newSeed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,93 @@ Stack.destroy_all
 
 puts "Destroy all posts, users, stacks, companies"
 
+job_titles = [
+  "Développeur Web Full Stack",
+  "Développeur Full Stack Senior",
+  "Ingénieur Full Stack",
+  "Développeur Full Stack",
+  "Lead Développeur Web Full Stack",
+  "Expert Full Stack",
+  "Développeur Full Stack - Projets Innovants",
+  "Architecte Full Stack",
+  "Développeur Full Stack - Start-Up Tech",
+  "Spécialiste Full Stack",
+  "Développeur Full Stack Remote",
+  "Développeur Full Stack pour Applications Mobiles",
+  "Développeur Full Stack E-commerce",
+  "Ingénieur Logiciel Full Stack",
+  "Développeur Full Stack - Solutions Cloud",
+  "Concepteur Full Stack - Plateformes Digitales",
+  "Développeur Full Stack Agile",
+  "Développeur Full Stack - Secteur FinTech",
+  "Développeur Full Stack - Projets Blockchain",
+  "Lead Technique Full Stack",
+  "Développeur Full Stack - Intelligence Artificielle",
+  "Spécialiste Full Stack - Sécurité Web",
+  "Développeur Full Stack - Jeux en Ligne",
+  "Consultant Full Stack",
+  "Développeur Full Stack - Technologies Émergentes"
+]
+contract_types = [
+  "CDI",
+  "CDD - 6 mois",
+  "CDD - 12 mois",
+  "Contrat freelance",
+  "CDD - 24 mois"
+]
+
+location = [
+  "Lyon",
+  "Paris",
+  "Nantes",
+  "Marseille",
+  "Bordeaux",
+  "Télétravail"
+]
+
+comp = [
+"Eiffage",
+"Solvay S.A.",
+"Blablacar",
+"Dassault Systèmes",
+"OVHcloud",
+"L'Oréal",
+"Capgemini",
+"Atos",
+"Sopra Steria",
+"Orange",
+"BNP Paribas",
+"AXA",
+"Ubisoft",
+"Publicis Groupe",
+"Thales",
+"ENGIE",
+"SNCF",
+"Danone",
+"Veolia Environnement",
+"Carrefour",
+"Accor",
+"Airbus",
+"Renault",
+"TotalEnergies",
+"Veepee"
+]
+
+stac = [
+"rails",
+"ruby",
+"javascript",
+"typescript",
+"gitlab",
+"github",
+"mysql",
+"postgresql",
+"vscode",
+"html5",
+"css3",
+"tailwindcss"
+]
+
 user = User.create!(
   email: 'a@a.com',
   password: '123456',
@@ -25,16 +112,18 @@ user = User.create!(
 )
 puts "#{user.email} - #{user.password} "
 
-50.times do
-  faker_company = Faker::Company.name
-  faker_location = Faker::Address.city
-  faker_title = Faker::Job.title
-  logo = "https://media.licdn.com/dms/image/C560BAQEbIYAkAURcYw/company-logo_100_100/0/1650566107463/canonical_logo?e=1717632000&v=beta&t=Iw7KBd9gVHxKGYMsE5DFmcCSzazrARLeeXPJomYeqmA"
-  faker_date = Faker::Date.between(from: 15.days.ago, to: Date.today)
-  faker_contract_type = Faker::Job.employment_type
-  faker_stack = Faker::ProgrammingLanguage.name
+i = 10
 
-  company = Company.new(name: faker_company, logo_url: logo)
+25.times do
+  faker_company = comp.shuffle!.pop
+  faker_location = location.sample
+  faker_title = job_titles.sample
+  faker_date = Faker::Date.between(from: 15.days.ago, to: Date.today)
+  faker_contract_type = contract_types.sample
+  faker_stack = stac.sample(3)
+  p faker_stack
+
+  company = Company.new(name: faker_company)
   p company
   company.save!
 
@@ -43,10 +132,10 @@ puts "#{user.email} - #{user.password} "
     location: faker_location,
     contract_type: faker_contract_type,
     published_on: faker_date,
-    url: "https://www.linkedin.com/jobs/search/?currentJobId=2948680713&keywords=web%20developer&origin=BLENDED_SEARCH_RESULT_NAVIGATION_JOB_CARD",
-    description: "lorem ipsum",
+    url: "https://fr.indeed.com/?r=us&vjk=87aa6baacd5c5711&advn=12366446329411#{i}",
+    description: "Une entreprise leader dans les solutions technologiques innovantes, recherche un Développeur Full Stack passionné et expérimenté pour rejoindre notre équipe dynamique. Si vous êtes motivé par les défis techniques et souhaitez contribuer à la création de produits révolutionnaires, ce poste est pour vous.",
     experience_years: rand(0..4).to_i,
-    status: [0, 10, 30].sample.to_i,
+    status: [0, 10, 20, 30].sample.to_i,
     updated_at: faker_date
   )
   post.user = user
@@ -64,6 +153,13 @@ puts "#{user.email} - #{user.password} "
 
   post_stack.save!
 
+  faker_stack.each do |stack_name|
+    stack = Stack.find_or_create_by(name: stack_name)
+    post_stack = PostStack.new(post: post, stack: stack)
+    post_stack.save!
+  end
+
+  i += 1
 end
 
 puts "seed ok"
